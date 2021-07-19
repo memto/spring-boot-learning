dd8d211b902e1f0b010212f59cb4199d07e241bb
dvn0zzz <dvn0zzz@gmail.com>
2021-07-20 05:12:37 +0700

    format code

--- spring-boot-3-bean-life-cycle-@PostConstruct-@PreDestroy/src/main/java/me/loda/spring/beanlifecycle/Girl.java
+++ spring-boot-3-bean-life-cycle-@PostConstruct-@PreDestroy/src/main/java/me/loda/spring/beanlifecycle/Girl.java
@@ -26,12 +26,12 @@ import org.springframework.stereotype.Component;
 public class Girl {
 
     @PostConstruct
-    public void postConstruct(){
+    public void postConstruct() {
         System.out.println("\t>> Đối tượng Girl sau khi khởi tạo xong sẽ chạy hàm này");
     }
 
     @PreDestroy
-    public void preDestroy(){
+    public void preDestroy() {
         System.out.println("\t>> Đối tượng Girl trước khi bị destroy thì chạy hàm này");
     }
 }
